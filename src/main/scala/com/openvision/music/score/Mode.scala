faf429cfe8212672c2207f897a0a402b77bf4639
Hannes Widmoser <widmoser@gmail.com>
2014-11-09 21:20:10 +0100

    added scales

--- src/main/scala/com/openvision/music/score/Mode.scala
+++ src/main/scala/com/openvision/music/score/Mode.scala
@@ -1,6 +1,52 @@
 package com.openvision.music.score
 
-sealed trait Mode
+import Interval._
 
-object Major extends Mode
-object Minor extends Mode
+sealed trait Mode {
+
+  def scale(name: NoteName): List[NoteName]
+
+}
+
+
+sealed trait HalfToneMode extends Mode {
+
+  protected def steps: List[Interval]
+
+  override def scale(name: NoteName): List[NoteName] = {
+    steps.foldLeft(List[NoteName](name)) { (list, interval) =>
+      list match {
+        case (x :: xs) => (x + interval) :: x :: xs
+      }
+    }.reverse
+  }
+
+}
+
+object Major extends HalfToneMode {
+
+  override val steps = List(
+    major(2),
+    major(2),
+    minor(2),
+    major(2),
+    major(2),
+    major(2),
+    minor(2)
+  )
+
+}
+
+object Minor extends HalfToneMode {
+
+  override val steps = List(
+    major(2),
+    minor(2),
+    major(2),
+    major(2),
+    minor(2),
+    major(2),
+    major(2)
+  )
+
+}
